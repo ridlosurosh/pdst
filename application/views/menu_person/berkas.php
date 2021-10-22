9e680470b52d5c8e12e99d76a5f7ba2fb318b413
muhlis99 <72523345+muhlis99@users.noreply.github.com>
2021-10-23 01:35:23 +0700

    Update berkas.php
    membuat satu kali upload

--- application/views/menu_person/berkas.php
+++ application/views/menu_person/berkas.php
@@ -9,30 +9,31 @@
 </section>
 <section class="content">
     <div class="container-fluid mb-3">
+        <form id="form_upload">
         <div class="card">
             <div class="card-header">
-                <button class="btn btn-sm btn-danger" onclick="menu_santri()"><i class="fas fa-reply"></i> Kembali Ke Data Santri</button>
+                <a href="#" class="btn btn-sm btn-danger" onclick="menu_santri()"><i class="fas fa-reply"></i> Kembali Ke Data Santri</a>
+                <button type="submit" class="btn btn-sm bg-gradient-primary float-right"> <i class="fa fa-upload"></i> Simpan</button>
+            
             </div>
+
+                <input type="hidden" name="idperson" value="<?= $id_person ?>">
             <div class="card-body">
                 <div class="row mt-4">
                     <div class="col-sm-6">
                         <div class="card">
                             <div class="card-body">
-                                <form id="form_upload">
-                                    <input type="hidden" name="idperson" value="<?= $id_person ?>">
                                     <div class="form-group">
                                         <label for="" class="col-form-label">Foto Santri</label><br>
-                                        <img src="#" id="gambar_foto" width="300" alt="Preview Gambar"><br>
+                                        <img src="<?= site_url() ?>../gambar/foto/<?= $foto_warna_santri ?>" id="gambar_foto" width="300" alt="Preview Gambar"><br><br>
                                         <input type="file" name="foto" id="preview_foto"><br><br>
-                                        <button class="btn btn-xs btn-primary" type="submit" id="btn-upload">Upload</button>
                                     </div>
-                                </form>
                             </div>
                         </div>
                     </div>
                     <script>
                         $(document).ready(function() {
-                            $('#gambar_foto').hide()
+                            // $('#gambar_foto').hide()
 
                             function bacaGambar(input) {
                                 if (input.files && input.files[0]) {
@@ -50,46 +51,23 @@
                                 bacaGambar(this);
                             });
 
-                            $('#form_upload').submit(function(e) {
-                                e.preventDefault();
-                                $.ajax({
-                                    url: '<?php echo site_url('Cperson/simpan_berkas') ?>',
-                                    type: "post",
-                                    data: new FormData(this),
-                                    processData: false,
-                                    contentType: false,
-                                    cache: false,
-                                    async: false,
-                                    success: function(data) {
-                                        swal.fire({
-                                            title: "PDST NAA",
-                                            text: "Foto Berhasil Diupload",
-                                            type: "success"
-                                        })
-                                    }
-                                });
-                            });
                         })
                     </script>
                     <div class="col-sm-6">
                         <div class="card">
                             <div class="card-body">
-                                <form id="form_upload4">
-                                    <input type="hidden" name="idperson" value="<?= $id_person ?>">
                                     <div class="form-group">
                                         <label for="" class="col-form-label">Foto Scan Akta Kelahiran</label><br>
-                                        <img src="#" id="gambar_akta" width="300" alt="Preview Gambar"><br>
+                                        <img src="<?= site_url() ?>../gambar/akta/<?= $foto_scan_akta ?>" id="gambar_akta" width="300" alt="Preview Gambar"><br><br>
                                         <input type="file" name="akta" id="preview_akta"><br><br>
-                                        <button class="btn btn-xs btn-primary" type="submit" id="btn-upload">Upload</button>
                                     </div>
-                                </form>
                             </div>
                         </div>
                     </div>
                 </div>
                 <script>
                     $(document).ready(function() {
-                        $('#gambar_akta').hide()
+                        // $('#gambar_akta').hide()
 
                         function bacaGambar(input) {
                             if (input.files && input.files[0]) {
@@ -112,21 +90,17 @@
                     <div class="col-sm-6">
                         <div class="card">
                             <div class="card-body">
-                                <form id="form_upload2">
-                                    <input type="hidden" name="idperson" value="<?= $id_person ?>">
                                     <div class="form-group">
                                         <label for="" class="col-form-label">Foto Scan Kartu Keluarga</label><br>
-                                        <img src="#" id="gambar_kk" width="300" alt="Preview Gambar"><br>
+                                        <img src="<?= site_url() ?>../gambar/kk/<?= $foto_scan_kk ?>" id="gambar_kk" width="300" alt="Preview Gambar"><br><br>
                                         <input type="file" name="kk" id="preview_kk"><br><br>
-                                        <button class="btn btn-xs btn-primary" type="submit" id="btn-upload">Upload</button>
                                     </div>
-                                </form>
                             </div>
                         </div>
                     </div>
                     <script>
                         $(document).ready(function() {
-                            $('#gambar_kk').hide()
+                            // $('#gambar_kk').hide()
 
                             function bacaGambar(input) {
                                 if (input.files && input.files[0]) {
@@ -148,22 +122,18 @@
                     <div class="col-sm-6">
                         <div class="card">
                             <div class="card-body">
-                                <form id="form_upload5">
-                                    <input type="hidden" name="idperson" value="<?= $id_person ?>">
                                     <div class="form-group">
                                         <label for="" class="col-form-label">Foto Scan SKCK</label><br>
-                                        <img src="#" id="gambar_skck" width="300" alt="Preview Gambar"><br>
+                                        <img src="<?= site_url() ?>../gambar/skck/<?= $foto_scan_skck ?>" id="gambar_skck" width="300" alt="Preview Gambar"><br><br>
                                         <input type="file" name="skck" id="preview_skck"><br><br>
-                                        <button class="btn btn-xs btn-primary" type="submit" id="btn-upload">Upload</button>
                                     </div>
-                                </form>
                             </div>
                         </div>
                     </div>
                 </div>
                 <script>
                     $(document).ready(function() {
-                        $('#gambar_skck').hide()
+                        // $('#gambar_skck').hide()
 
                         function bacaGambar(input) {
                             if (input.files && input.files[0]) {
@@ -186,21 +156,17 @@
                     <div class="col-sm-6">
                         <div class="card">
                             <div class="card-body">
-                                <form id="form_upload7">
-                                    <input type="hidden" name="idperson" value="<?= $id_person ?>">
                                     <div class="form-group">
                                         <label for="" class="col-form-label">Foto Wali Santri</label><br>
-                                        <img src="#" id="gambar_wali" width="300" alt="Preview Gambar"><br>
+                                        <img src="<?= site_url() ?>../gambar/wali/<?= $foto_wali_santri_warna ?>" id="gambar_wali" width="300" alt="Preview Gambar"><br><br>
                                         <input type="file" name="wali" id="preview_wali"><br><br>
-                                        <button class="btn btn-xs btn-primary" type="submit" id="btn-upload">Upload</button>
                                     </div>
-                                </form>
                             </div>
                         </div>
                     </div>
                     <script>
                         $(document).ready(function() {
-                            $('#gambar_wali').hide()
+                            // $('#gambar_wali').hide()
 
                             function bacaGambar(input) {
                                 if (input.files && input.files[0]) {
@@ -222,21 +188,17 @@
                     <div class="col-sm-6">
                         <div class="card">
                             <div class="card-body">
-                                <form id="form_upload6">
-                                    <input type="hidden" name="idperson" value="<?= $id_person ?>">
                                     <div class="form-group">
                                         <label for="" class="col-form-label">Foto Scan Keterangan Sehat</label><br>
-                                        <img src="#" id="gambar_sukes" width="300" alt="Preview Gambar"><br>
+                                        <img src="<?= site_url() ?>../gambar/sukes/<?= $foto_scan_ket_sehat ?>" id="gambar_sukes" width="300" alt="Preview Gambar"><br><br>
                                         <input type="file" name="ket_sehat" id="preview_sukes"><br><br>
-                                        <button class="btn btn-xs btn-primary" type="submit" id="btn-upload">Upload</button>
                                     </div>
-                                </form>
                             </div>
                         </div>
                     </div>
                     <script>
                         $(document).ready(function() {
-                            $('#gambar_sukes').hide()
+                            // $('#gambar_sukes').hide()
 
                             function bacaGambar(input) {
                                 if (input.files && input.files[0]) {
@@ -254,18 +216,23 @@
                                 bacaGambar(this);
                             });
                         })
+
                     </script>
                 </div>
             </div>
+
+            </form>
+
         </div>
     </div>
+
 </section>
 <script>
     $(document).ready(function() {
-        $('#form_upload2').submit(function(e) {
+        $('#form_upload').submit(function(e) {
             e.preventDefault();
             $.ajax({
-                url: '<?php echo site_url('Cperson/simpan_berkas_kk') ?>',
+                url: '<?php echo site_url('Cperson/simpan_berkas') ?>',
                 type: "post",
                 data: new FormData(this),
                 processData: false,
@@ -283,131 +250,4 @@
         });
     });
 
-    $(document).ready(function() {
-        $('#form_upload3').submit(function(e) {
-            e.preventDefault();
-            $.ajax({
-                url: '<?php echo site_url('Cperson/simpan_berkas_ijz') ?>',
-                type: "post",
-                data: new FormData(this),
-                processData: false,
-                contentType: false,
-                cache: false,
-                async: false,
-                success: function(data) {
-                    alert("upload berhasil");
-                }
-            });
-        });
-    });
-
-    $(document).ready(function() {
-        $('#form_upload4').submit(function(e) {
-            e.preventDefault();
-            $.ajax({
-                url: '<?php echo site_url('Cperson/simpan_berkas_akta') ?>',
-                type: "post",
-                data: new FormData(this),
-                processData: false,
-                contentType: false,
-                cache: false,
-                async: false,
-                success: function(data) {
-                    swal.fire({
-                        title: "PDST NAA",
-                        text: "Berkas Akta Berhasil Diupload",
-                        type: "success"
-                    })
-                }
-            });
-        });
-    });
-
-    $(document).ready(function() {
-        $('#form_upload5').submit(function(e) {
-            e.preventDefault();
-            $.ajax({
-                url: '<?php echo site_url('Cperson/simpan_berkas_skck') ?>',
-                type: "post",
-                data: new FormData(this),
-                processData: false,
-                contentType: false,
-                cache: false,
-                async: false,
-                success: function(data) {
-                    swal.fire({
-                        title: "PDST NAA",
-                        text: "Berkas SKCK Berhasil Diupload",
-                        type: "success"
-                    })
-                }
-            });
-        });
-    });
-
-    $(document).ready(function() {
-        $('#form_upload6').submit(function(e) {
-            e.preventDefault();
-            $.ajax({
-                url: '<?php echo site_url('Cperson/simpan_berkas_ket_sehat') ?>',
-                type: "post",
-                data: new FormData(this),
-                processData: false,
-                contentType: false,
-                cache: false,
-                async: false,
-                success: function(data) {
-                    swal.fire({
-                        title: "PDST NAA",
-                        text: "Berkas Keterangan Sehat Berhasil Diupload",
-                        type: "success"
-                    })
-                }
-            });
-        });
-    });
-
-    $(document).ready(function() {
-        $('#form_upload7').submit(function(e) {
-            e.preventDefault();
-            $.ajax({
-                url: '<?php echo site_url('Cperson/simpan_berkas_foto_wl') ?>',
-                type: "post",
-                data: new FormData(this),
-                processData: false,
-                contentType: false,
-                cache: false,
-                async: false,
-                success: function(data) {
-                    swal.fire({
-                        title: "PDST NAA",
-                        text: "Berkas Wali Berhasil Diupload",
-                        type: "success"
-                    })
-                }
-            });
-        });
-    });
-
-    // $(Document).ready(function() {
-    //     $('#foto').on('change', function() {
-    //         var foto = (this).files[0];
-    //         viewImages(foto);
-    //     })
-
-    //     $('#kk').on('change', function() {
-    //         var kk = (this).files[0];
-    //         viewImages(kk);
-    //     })
-
-    // })
-
-    // function viewImages(file) {
-    //     var reader = new FileReader();
-    //     reader.onload = function() {
-    //         $('#fotos').attr('src', reader.result);
-    //         $('#kaka').attr('src', reader.result);
-    //     }
-    //     reader.readAsDataURL(file);
-    // }
 </script>
