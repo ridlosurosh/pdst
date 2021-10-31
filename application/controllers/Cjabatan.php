fcec340a41d9f8a2aab5ace6c68ac4b0a81ca9fb
muhlis99 <72523345+muhlis99@users.noreply.github.com>
2021-11-01 00:42:38 +0700

    Update Cjabatan.php

--- application/controllers/Cjabatan.php
+++ application/controllers/Cjabatan.php
@@ -24,10 +24,10 @@
         {
             $jbtn = $this->input->post('nama_jabatan');
             $jabatan = $this->db->where('nm_jabatan', $jbtn)
-                            ->where('status','aktif')
-                            ->get('tb_jabatan')
-                            ->num_rows();
-            if ($jabatan > 0 ) {
+                ->where('status', 'aktif')
+                ->get('tb_jabatan')
+                ->num_rows();
+            if ($jabatan > 0) {
                 $pesan = "tidak";
                 $sukses = " Jabatan Sudah Ada ";
             } else {
@@ -79,5 +79,39 @@
                 'sukses' => $sukses
             );
             echo json_encode($output);
+        }
+
+        public function detail()
+        {
+            $id = $this->input->post("id");
+            $data = $this->Mjabatan->detail($id);
+            if (count($data) > 0) {
+                $no = 1;
+                foreach ($data as $value) {
+                    $row = array();
+                    $row[] = '<tr style="color: dimgrey;">';
+                    $row[] = '<td>' . $no++ . '</td>';
+                    $row[] = '<td>' . $value->niup . '</td>';
+                    $row[] = '<td>' . $value->nama . '</td>';
+                    $row[] = '<td>' . $value->alamat_lengkap . '</td>';
+                    $row[] = '<td>' . $value->nm_jabatan . '</td>';
+                    if ($value->sts === "aktif") {
+                        $row[] = '<td>' . '<span class="badge bg-primary">' .  $value->sts . '</span>' . '</td>';
+                    } else {
+                        $row[] = '<td>' . '<span class="badge bg-danger">' .  $value->sts . '</span>' . '</td>';
+                    }
+                    $row[] = '</tr>';
+                    $data[] = $row;
+                }
+                $sukses = "ya";
+            } else {
+                $sukses = "tidak";
+                $data = '<tr><td colspan="10" class="text-center"><span class="text-danger" style="text-transform: uppercase"> Data kosong boss </span>  </td></tr>';
+            }
+            $out = array(
+                'pesan' => $sukses,
+                'list_detail' => $data
+            );
+            echo json_encode($out);
         }
     }
