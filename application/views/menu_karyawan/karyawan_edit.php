002e4e414653f520afe1973eb196afb8eee16d4e
muhlis99 <72523345+muhlis99@users.noreply.github.com>
2021-11-02 19:48:53 +0700

    Update karyawan_edit.php

--- application/views/menu_karyawan/karyawan_edit.php
+++ application/views/menu_karyawan/karyawan_edit.php
@@ -13,8 +13,8 @@
             <div class="card-header">
                 <h3 class="card-title">Edit Karyawan</h3>
             </div>
-            <div class="card-body">
-                <form id="edit_karyawan">
+            <form id="edit_karyawan">
+                <div class="card-body">
                     <div class="row">
                         <div class="col-md-6">
                             <input type="hidden" name="idkaryawan" value="<?= $id_karyawan ?>">
@@ -34,7 +34,7 @@
                                         <div class="input-group-prepend">
                                             <span class="input-group-text"><i class="far fa-calendar-alt"></i></span>
                                         </div>
-                                        <input type="text" name="tanggal_diangkat" class="form-control" id="pengangkatan" value="<?= $tgl_diangkat ?>">
+                                        <input type="text" name="tanggal_diangkat" class="form-control" id="pengangkatan" value="<?= $tgl_diangkat ?>" autocomplete="off">
                                     </div>
                                 </div>
                             </div>
@@ -75,12 +75,12 @@
                             </div>
                         </div>
                     </div>
-                </form>
-            </div>
-            <div class="card-footer">
-                <button class="btn btn-sm btn-default" onclick="karyawan()">Keluar</button>
-                <button class="btn btn-sm btn-primary float-right" onclick="edit_karyawan()"><i class="fas fa-edit"></i> Simpan</button>
-            </div>
+                </div>
+                <div class="card-footer">
+                    <button type="button" class="btn btn-sm btn-default bg-danger" onclick="karyawan()"><i class="fas fa-reply"></i> Keluar</button>
+                    <button class="btn btn-sm btn-primary float-right"><i class="fas fa-save"></i> Simpan</button>
+                </div>
+            </form>
         </div>
     </div>
 </section>
@@ -111,25 +111,87 @@
 
     })
 
-    function edit_karyawan() {
-        $.ajax({
-            url: "<?= site_url('Ckaryawan/edit_karyawan') ?>",
-            data: $('#edit_karyawan').serialize(),
-            type: 'POST',
-            dataType: 'JSON',
-            success: function(data) {
-                if (data.pesan === "ya") {
-                    swal.fire({
-                        title: "PDST NAA",
-                        text: data.sukses,
-                        type: "success"
-                    }).then(okay => {
-                        if (okay) {
-                            karyawan()
-                        }
-                    })
+    $.validator.addMethod("valueNotEquals", function(value, element, arg) {
+        return arg !== value;
+    }, "Value must not equal arg.");
+    $("select").on("select2:close", function(e) {
+        $(this).valid();
+    });
+    $('#edit_karyawan').validate({
+        rules: {
+            nama_pengajar: {
+                required: true
+            },
+            alamat_lengkap: {
+                required: true
+            },
+            tanggal_diangkat: {
+                required: true
+            },
+            nama_wilayah: {
+                valueNotEquals: "default"
+            },
+            angkat: {
+                valueNotEquals: "0"
+            },
+            instansi: {
+                valueNotEquals: "0"
+            },
+            tanggal_berhenti: {
+                required: true
+            },
+        },
+        messages: {
+            nama_pengajar: {
+                required: "Tidak Boleh Kosong"
+            },
+            alamat_lengkap: {
+                required: "Tidak Boleh Kosong"
+            },
+            tanggal_diangkat: {
+                required: "Tidak Boleh Kosong"
+            },
+            nama_wilayah: {
+                valueNotEquals: "Tidak Boleh Kosong"
+            },
+            angkat: {
+                valueNotEquals: "Tidak Boleh Kosong"
+            },
+            instansi: {
+                valueNotEquals: "Tidak Boleh Kosong"
+            },
+            tanggal_berhenti: {
+                required: "Tidak Boleh Kosong"
+            },
+        },
+        errorElement: 'span',
+        errorPlacement: function(error, element) {
+            error.addClass('invalid-feedback');
+            element.closest('.form-group').append(error);
+        },
+        unhighlight: function(element, errorClass, validClass) {
+            $(element).removeClass('is-invalid');
+        },
+        submitHandler: function() {
+            $.ajax({
+                url: "<?= site_url('Ckaryawan/edit_karyawan') ?>",
+                data: $('#edit_karyawan').serialize(),
+                type: 'POST',
+                dataType: 'JSON',
+                success: function(data) {
+                    if (data.pesan === "ya") {
+                        swal.fire({
+                            title: "PDST NAA",
+                            text: data.sukses,
+                            type: "success"
+                        }).then(okay => {
+                            if (okay) {
+                                karyawan()
+                            }
+                        })
+                    }
                 }
-            }
-        });
-    }
+            });
+        }
+    })
 </script>
