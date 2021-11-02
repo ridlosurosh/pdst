2044c406f2ebfc5ff1ee8e4621e790b7fd46f795
muhlis99 <72523345+muhlis99@users.noreply.github.com>
2021-11-02 19:53:26 +0700

    Update pengajar_edit.php

--- application/views/menu_pengajar/pengajar_edit.php
+++ application/views/menu_pengajar/pengajar_edit.php
@@ -33,7 +33,7 @@
                                         <div class="input-group-prepend">
                                             <span class="input-group-text"><i class="far fa-calendar-alt"></i></span>
                                         </div>
-                                        <input type="text" class="form-control" name="tgl_diangkat" id="tgl" placeholder="" value="<?= $tgl_diangkat ?>">
+                                        <input type="text" class="form-control" name="tgl_diangkat" id="tgl" placeholder="" value="<?= $tgl_diangkat ?>" autocomplete="off">
                                     </div>
                                 </div>
                             </div>
