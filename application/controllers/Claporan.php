d0d26a7ef7d06432f8aa662827d19d16d1863ede
ridlosurosh <92989674+ridlosurosh@users.noreply.github.com>
2021-11-03 03:17:47 +0700

    Update Claporan.php

--- application/controllers/Claporan.php
+++ application/controllers/Claporan.php
@@ -8,28 +8,24 @@ class Claporan extends CI_Controller
     {
         parent::__construct();
         $this->load->model('Mlaporan');
-        $this->load->model('Mkamar');
+        // $this->load->model('Mkamar');
     }
 
     public function santri_provinsi()
     {
-        $arrayName = array(
-            'provinsi' => $this->Mlaporan->get_provinsi()->result(),
-            'santri' => $this->Mlaporan->santri_all()
-        );
-        
-        $this->load->view('laporan/santri_provinsi', $arrayName);
+        $output['provinsi'] = $this->Mlaporan->get_provinsi()->result();
+        $this->load->view('laporan/santri_provinsi', $output);
     }
 
-    public function santri_kamar()
-    {
-        $arrayName = array(
-            'kamar' => $this->Mkamar->kamar_all(),
-            'santrinya' => $this->Mlaporan->data_history()
-        );
-        
-        $this->load->view('laporan/person_perkamar', $arrayName);
-    }
+    // public function santri_kamar()
+    // {
+    //     $arrayName = array(
+    //         'kamar' => $this->Mkamar->kamar_all(),
+    //         'santrinya' => $this->Mlaporan->data_history()
+    //     );
+
+    //     $this->load->view('laporan/person_perkamar', $arrayName);
+    // }
 
 
 }
