33743ceb9d3ae449614677ce972e9874ceba0abb
muhlis99 <72523345+muhlis99@users.noreply.github.com>
2021-11-07 03:15:11 +0700

    Update Mgrafik.php

--- application/models/Mgrafik.php
+++ application/models/Mgrafik.php
@@ -9,34 +9,9 @@ class Mgrafik extends CI_Model
         return $query->result();
     }
 
-    // public function grafik_filter($tahun)
-    // {
-    //     $this->db->where('YEAR(tanggal_kunjungan)', date($tahun));
-    //     $this->db->select('monthname(tanggal_kunjungan) as bulan');
-    //     $this->db->select("count(id_person) as jml");
-    //     $this->db->group_by('month(tanggal_kunjungan)');
-    //     return $this->db->from('tb_kunjungan_santri')->get()->result();
-    // }
-
-    // public function grafik_filter_l($tahun, $jenis)
-    // {
-    //     $this->db->join('tb_person', 'tb_person.id_person=tb_kunjungan_santri.id_person');
-    //     $this->db->where('YEAR(tanggal_kunjungan)', date($tahun));
-    //     $this->db->where('jenis_kelamin', $jenis);
-    //     $this->db->select('monthname(tanggal_kunjungan) as bulan');
-    //     $this->db->select("count(tb_kunjungan_santri.id_person) as jml_lk");
-    //     $this->db->group_by('month(tanggal_kunjungan)');
-    //     return $this->db->from('tb_kunjungan_santri')->get()->result();
-    // }
-
-    // public function grafik_filter_p($tahun, $jenis)
-    // {
-    //     $this->db->join('tb_person', 'tb_person.id_person=tb_kunjungan_santri.id_person');
-    //     $this->db->where('YEAR(tanggal_kunjungan)', date($tahun));
-    //     $this->db->where('jenis_kelamin', $jenis);
-    //     $this->db->select('monthname(tanggal_kunjungan) as bulan');
-    //     $this->db->select("count(tb_kunjungan_santri.id_person) as jml_pr");
-    //     $this->db->group_by('month(tanggal_kunjungan)');
-    //     return $this->db->from('tb_kunjungan_santri')->get()->result();
-    // }
+    public function grafik_perwilayah($tahun)
+    {
+        $query = $this->db->query("SELECT tb_wilayah.nama_wilayah,COUNT(tb_kamar.id_kamar) as jml from tb_history,tb_kamar,tb_wilayah,tb_blok where tb_history.id_kamar=tb_kamar.id_kamar AND tb_kamar.id_blok=tb_blok.id_blok AND tb_blok.id_wilayah=tb_wilayah.id_wilayah AND tb_history.aktif='ya' AND Year(tb_history.tgl_penetapan) = $tahun GROUP BY nama_wilayah");
+        return $query->result();
+    }
 }
