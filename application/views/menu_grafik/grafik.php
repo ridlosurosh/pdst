75f9aeaaaa242898f37d630741f4544fff3bd0aa
ridlosurosh <92989674+ridlosurosh@users.noreply.github.com>
2021-11-07 02:01:23 +0700

    Create grafik.php

---
+++ application/views/menu_grafik/grafik.php
@@ -0,0 +1,14 @@
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0 text-dark">Grafik</h1>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="content">
+    <div class="container-fluid">
+        <div class="row"></div>
+    </div>
+</section>
