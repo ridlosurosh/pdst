6c582fad392806b55b4e56f875e905df6b0fbffd
muhlis99 <72523345+muhlis99@users.noreply.github.com>
2021-11-07 02:46:28 +0700

    Update Cgrafik.php

--- application/controllers/Cgrafik.php
+++ application/controllers/Cgrafik.php
@@ -13,4 +13,58 @@ class Cgrafik extends CI_Controller
     {
         $this->load->view('menu_grafik/grafik');
     }
+
+    function grafik_tahun()
+    {
+        $data = $this->Mgrafik->grafik_tahun();
+        if ($data == null) {
+            $da[] = '0';
+            $dat[] = 'Harap Di Isi Bos qqq Kunjungan Pertahun Lagi KOsong OK HEHE';
+        } else {
+            foreach ($data as $value) {
+                $dat[] = $value->tahun;
+                $da[] = $value->jml;
+            }
+        }
+        echo json_encode(array('tahun' => $dat, 'jml' => $da));
+    }
+
+    // function grafik_filter_putra_putri()
+    // {
+    //     $bulan = $this->input->post('bulan');
+    //     $tahun = $this->input->post('tahun');
+    //     $data = $this->Mgrafik->grafik_filter_putra_putri($tahun, $bulan);
+    //     if ($data == null) {
+    //         $date[] = '0';
+    //         $dat[] = 'harap di isi bos qqq Kunjungan PUtra Putri  Lagi KOsong OK';
+    //     } else {
+    //         foreach ($data as $value) {
+    //             $dat[] = $value->nama_wilayah;
+    //             $date[] = $value->jml;
+    //         }
+    //     }
+    //     echo json_encode(array('wilayah' => $dat, 'jml' => $date));
+    // }
+
+    // function grafik_filter_putra_putri_pertahun()
+    // {
+    //     $tahun = $this->input->post('tahun');
+    //     $data = $this->Mgrafik->grafik_filter_l($tahun, 'Laki-Laki');
+    //     $da = $this->Mgrafik->grafik_filter_p($tahun, 'Perempuan');
+    //     if ($data == null) {
+    //         $date_lk[] = '0';
+    //         $date_pr[] = '0';
+    //         $dat[] = 'Harap Di Isi Bos qqq Kunjungan Pertahun Lagi KOsong OK HEHE';
+    //     } else {
+    //         foreach ($data as $value) {
+    //             $dat[] = $value->bulan;
+    //             $date_lk[] = $value->jml_lk;
+    //         }
+    //         foreach ($da as $value) {
+    //             // $dat[] = $value->bulan;
+    //             $date_pr[] = $value->jml_pr;
+    //         }
+    //     }
+    //     echo json_encode(array('bulan' => $dat, 'jml_lk' => $date_lk, 'jml_pr' => $date_pr));
+    // }
 }
